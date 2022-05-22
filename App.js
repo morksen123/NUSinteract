c5009ca4effb442599f6d6f75d548d8991c30305
morksen123 <morksen123@gmail.com>
2022-05-22 16:30:23 +0800

    test github

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Orbital 2022 2.0</Text>
+      <Text>Orbital 2022 2.01</Text>
       <StatusBar style="auto" />
     </View>
   );
