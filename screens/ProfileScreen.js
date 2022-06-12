e4d4ab8cd06a02ede54a03bea42054eee5799b30
morksen123 <morksen123@gmail.com>
2022-06-12 15:24:28 +0800

    pulled username

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,73 +1,52 @@
-import { Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Alert, Text } from "react-native";
+import { Button, Input } from "react-native-elements";
+import { supabase } from '../utils/supabase';
 
 
-const ProfileScreen = () => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Profile Screen</Text>
-        </View>  
-    )
+
+export default function ProfileScreen() {
+  const [user, setUser] = useState("");
+
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    const session = supabase.auth.session(); 
+    setSession(session)
+    setUser(session?.user ?? null);
+
+    const { data: authListender } = supabase.auth.onAuthStateChange(
+        (event, session) => {
+            console.log(event)
+            setSession(session)
+            setUser(session?.user ?? null);
+        })
+
+        return () => {
+            authListender.unsubscribe();
+        }
+  }, [])
+  console.log(session)
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>{session ? user.user_metadata.username : 'nothing'}</Text>
+    </View>
+  );
 }
 
-export default ProfileScreen; 
 
-
-// import { useState, useEffect } from "react";
-// import { StyleSheet, View, Alert } from "react-native";
-// import { Button, Input } from "react-native-elements";
-// import { ApiError, Session } from "@supabase/supabase-js";
-// import { supabase } from '../utils/supabase';
-
-
-
-// export default function ProfileScreen({ session }) {
-//   const [username, setUsername] = useState("");
-
-//   useEffect(() => {
-//     if (session) getProfile();
-//   }, [session]);
-
-//   async function getProfile() {
-//     try {
-//       const user = supabase.auth.user();
-//       if (!user) throw new Error("No user on the session!");
-
-//       let { data, error, status } = await supabase
-//         .from("profiles")
-//         .select(`username, website, avatar_url`)
-//         .eq("id", user.id)
-//         .single();
-//       if (error && status !== 406) {
-//         throw error;
-//       }
-
-//       if (data) {
-//         setUsername(data.username);
-//       }
-//     } catch (error) {
-//       Alert.alert(error.message);
-//     } 
-//   }
-
-
-//   return (
-//     <Text>{supabase.auth.user()}</Text>
-      
-//   );
-// }
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     marginTop: 40,
-//     padding: 12,
-//   },
-//   verticallySpaced: {
-//     paddingTop: 4,
-//     paddingBottom: 4,
-//     alignSelf: "stretch",
-//   },
-//   mt20: {
-//     marginTop: 20,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: "stretch",
+  },
+  mt20: {
+    marginTop: 20,
+  },
+});
--- utils/supabase.js
+++ utils/supabase.js
@@ -5,3 +5,5 @@ const supabaseAnonKey =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxZW9wZGtrZmhyYWR0bGV6cGlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM0NTIxMTEsImV4cCI6MTk2OTAyODExMX0.MZZovcPnuGFnM2wDyabFZAuL8ei9vZqlfxql4I849wA"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
