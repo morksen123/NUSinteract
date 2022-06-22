da1d55f73c9dae187a4e8cc85a498b0faa9d8b69
morksen123 <morksen123@gmail.com>
2022-06-23 01:37:47 +0800

    chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,75 @@
-import { Text, View } from 'react-native';
+import {
+    Text, 
+    View,
+    SafeAreaView, 
+    TextInput, 
+    StyleSheet, 
+    Button 
+} from 'react-native';
+import { supabase } from '../utils/supabase';
+import { useState } from 'react';
+
+import Messages from '../components/messages/messages';
+
+const THEME = '#3F3F3F'
 
 const ChatScreen = () => {
+
+    const [message, setMessage] = useState(''); 
+
+    const onPressHandler = async () => {
+
+        const { error, data } = await supabase
+            .from('messages')
+            .insert({ 
+                content: message,
+                user_id: supabase.auth.user().id
+             })
+
+            //  console.log({ error, data })
+    }
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
             <Text>Chat Screen</Text>
-        </View>  
+            
+            <TextInput 
+                style={styles.textInput}
+                selectionColor={THEME}
+                value={message}
+                onChangeText={setMessage}
+            />
+
+            <Button onPress={onPressHandler} title="Send Message"/>
+            <Messages/>
+            
+        </SafeAreaView>  
     )
 }
 
 export default ChatScreen; 
+
+
+const styles = StyleSheet.create({
+
+    textInput: {
+        alignSelf: 'center',
+        borderWidth: 2,
+        borderColor: THEME, 
+        borderRadius: 4, 
+        width: '90%',
+        height: 40, 
+        paddingHorizontal: 8, 
+    },
+
+    
+    title: {
+        fontSize: 35, 
+        fontFamily: "AvenirNext-Italic",
+        textAlign: 'center',
+        marginBottom: 20
+    },
+
+    boldText: {
+        fontWeight: '500'
+    }
+});
